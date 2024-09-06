ad285f5df5c4859a492c57132b7fb8f21a918249
garry911128 <garry80633@gmail.com>
2024-09-06 22:41:17 +0800

    complete docker file

--- backend/main.go
+++ backend/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello from Go!")
+        fmt.Println("Received a request at /") // 這條語句會打印到容器日誌
     })
     http.ListenAndServe(":8080", nil)
 }
